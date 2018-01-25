0ca423621ec348c3e8c493d2b629bbcd47b3619c
Alex White <alexwhite335@gmail.com>
2018-01-26 10:10:00 +1300

    Turned out to be flappy bird but in a chem lab

--- MainWindow.py
+++ MainWindow.py
@@ -1,16 +1,43 @@
-#!/usr/bin/env python3
- 2 import tkinter as tk
- 3 
- 4 class Application(tk.Frame):
- 5     def __init__(self, master=None):
- 6         tk.Frame.__init__(self, master)
- 7         self.grid()  
- 8         self.createWidgets()
- 9 
-10     def createWidgets(self):
-11         self.quitButton = tk.Button(self, text='Quit', command=self.quit)
-12         self.quitButton.grid()
-13 
-14 app = Application()
-15 app.master.title('Sample application')
-16 app.mainloop()
+import pygame
+from pygame.locals import *
+import numpy as np
+
+
+pygame.init()
+screen_dims = (750,569)
+screen = pygame.display.set_mode(screen_dims)
+pygame.display.set_caption("Pygame Demo")
+
+bg = pygame.image.load("background.jpg").convert()
+donut = pygame.image.load("donut.png").convert_alpha()
+screen.blit(bg, (0, 0))
+
+donut_coords = np.array(screen_dims)[:]
+
+def pause_game():
+    return
+
+game_exit = False
+motion = False
+while not game_exit:
+    for event in pygame.event.get():
+    
+        if event.type == QUIT:
+            game_exit = True
+            
+        elif event.type == KEYDOWN:
+            keys = pygame.key.get_pressed()
+            if keys[K_ESCAPE]:
+                pause_game()
+                
+            if keys[K_UP]:
+                if not motion:
+                    motion = True
+                
+            # if keys[K_DOWN]:
+                
+    screen.blit(bg, (0,0))
+    screen.blit(donut, (donut_coords[0], donut_coords[1]))
+    pygame.display.update()
+pygame.quit()
+quit()
