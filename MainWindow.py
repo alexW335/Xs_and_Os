0284fbdd489afc2f136aa7d2ed15daaa6b9f9ecd
Alex White <alexwhite335@gmail.com>
2018-01-26 16:09:40 +1300

    working, now need to check for collision then done

--- MainWindow.py
+++ MainWindow.py
@@ -2,6 +2,8 @@ import pygame
 from pygame.locals import *
 import random
 
+thug_mode = False
+
 pygame.init()
 pygame.font.init()
 clock = pygame.time.Clock()
@@ -10,9 +12,13 @@ screen = pygame.display.set_mode(screen_dims)
 screen.fill([255, 255, 255])
 pygame.display.set_caption("Pygame Demo")
 
-bg = pygame.image.load("background.jpg").convert()
-donut = pygame.image.load("donut.png").convert_alpha()
-
+if thug_mode:
+    bg = pygame.image.load("./data/background.png").convert()
+    donut = pygame.image.load("./data/sprite2.png").convert_alpha()
+else:
+    bg = pygame.image.load("background.jpg").convert()
+    donut = pygame.image.load("beaker.png").convert_alpha()
+    
 screen.blit(bg, (0, 0))
 
 donut_coords = [screen_dims[0]/4, screen_dims[1]/2]
@@ -22,9 +28,10 @@ myfont = pygame.font.SysFont('Comic Sans MS', 50)
 # def display_msg(msg, col):
 
 pillar_timer = 0
-pillar_trigger = 50
+pillar_trigger = 60
 pillar_speed = 0
-pillar_gap = 25
+pillar_gap = 200
+pillar_width = 25
 pillars = []
 count = 0
 speed = 0
@@ -33,7 +40,7 @@ game_exit = False
 started = False
 
 while not game_exit:
-    tick_time = clock.tick(50)
+    tick_time = clock.tick(60)
     
     if started:
         pillar_timer = (pillar_timer+1) % pillar_trigger
@@ -56,32 +63,45 @@ while not game_exit:
                 if not started:
                     started = True
                 speed = 20
-                pillar_speed = 2
+                pillar_speed = 5
     
     
     if pillar_timer == pillar_trigger-1:
-        pillars.append([screen_dims[0], random.randrange(0+pillar_gap, screen_dims[1] - pillar_gap)])
+        pillars.append([screen_dims[0], random.randrange(0+screen_dims[1]//5, round(screen_dims[1]*0.8)), False])
         print(pillars)
     
     if started:
         if pillars:
+            if pillars[0][0] <= 0:
+                pillars = pillars[1:]
             for pil in pillars:
                 pil[0] -= pillar_speed
-            if pillars[0][0] <=0:
-                pillars = pillars[1:]
+                # pygame.draw.rect(screen, (25, 25, 25), [pil[0], pil[1], pillar_width, screen_dims[1]-pil[1]])
+    # if len(pillars) != 0:
+    
+            
         
     donut_coords[1] -= speed
     
+    screen.blit(bg, (0,0))
+    screen.blit(donut, (donut_coords[0], donut_coords[1]))
+    
+    
+    for pil in pillars:
+        if pil[0] <= donut_coords[0] and not pil[2]:
+            pil[2] = True
+            count += 1
+        screen.fill((75, 75, 75), [pil[0], pil[1], pillar_width, screen_dims[1]-pil[1]])
+        screen.fill((75, 75, 75), [pil[0], 0, pillar_width, pil[1]- pillar_gap])
     if donut_coords[1] >= screen_dims[1]-63:
         speed = 0
         pillar_speed = 0
         started = False
-        textsurface = myfont.render('You lose!', True, (255, 0, 0))
+        textsurface = myfont.render('You lose! You scored {}'.format(count), True, (255, 0, 0))
+        screen.blit(textsurface, (screen_dims[0]/5, screen_dims[1]*0.05))
     else:
         textsurface = myfont.render('{}'.format(count), True, (255, 0, 0))
-    screen.blit(bg, (0,0))
-    screen.blit(donut, (donut_coords[0], donut_coords[1]))
-    screen.blit(textsurface, (screen_dims[0]/2, screen_dims[1]*0.05))
-    pygame.display.update()
+        screen.blit(textsurface, (screen_dims[0]/2, screen_dims[1]*0.05))
+    pygame.display.flip()
 pygame.quit()
 quit()
